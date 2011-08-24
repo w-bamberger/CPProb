a56d9c61684f161b4e0baba28ffa6f776bd4649b
w-bamberger <w.bamberger@arcor.de>
2011-08-24 16:43:58 +0200

    Improved bag tests with new test for bag network with latent variables.
    * Renamed genBagData.py → gen-bag-data.py
    * Improved gen-bag-data.py to create two kinds of test data: One is
      called custom, the other one russel-norvig.
    * New test latent-bag. It contains a data file created with the option
      custom of gen-bag-data.py. It runs cpprobtest with the test case
      latent-bag-test. (latent-bag.csv contains 1000 records,
      latent-bag-long.csv 50000 records.)
    * To document the way the test data is created I wrote shell scripts
      called make-data-file.sh in the directories of both bag tests.

---
+++ test/bag/make-data-file.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# This file simply documents the command line arguments
+# used to great the data file.
+../../src-test/gen-bag-data.py russel-norvig bag.csv
---
+++ test/latent-bag/make-data-file.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# This file simply documents the command line arguments
+# used to great the data file.
+../../src-test/gen-bag-data.py custom latent-bag.csv
