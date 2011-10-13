8aaad0c9eefdeef228215de3fa68cde651a40dd4
w-bamberger <w.bamberger@arcor.de>
2011-10-13 17:43:05 +0200

    InfiniteBagTest: Fixture class and correct values depending on the file name.
    * The helper methods are now in a fixture class. This class also saves
      the constants (alpha) and the file name.
    * In the constructor, a correct value is determined for the flavor
      distribution. It is set dependent on the file name. Known files are
      latent-bag-short.csv, latent-bag.csv and latent-bag-long.csv.
    * The sampled distribution is compared with the correct
      distribution by BOOST_CHECK_CLOSE.

--- src-test/InfiniteBagTest.cpp
+++ src-test/InfiniteBagTest.cpp
@@ -10,6 +10,7 @@
 #include "../src-lib/RandomBoolean.hpp"
 #include <boost/program_options.hpp>
 #include <boost/range/adaptor/map.hpp>
+#include <boost/test/floating_point_comparison.hpp>
 #include <boost/test/unit_test.hpp>
 #include <boost/timer.hpp>
 
@@ -18,79 +19,159 @@ using namespace std;
 
 extern boost::program_options::variables_map options_map;
 
-BayesianNetwork
-gen_infinite_bag_net(float alpha, size_t lines_of_evidence =
-    std::numeric_limits<std::size_t>::max())
+class InfiniteBagFixture
 {
-  const DiscreteRandomReferences no_condition;
-  BayesianNetwork bn;
 
-  // Set up the parameter vertices
-  map<string, ConditionalDirichletNode*> params_table;
-  RandomBoolean bag("Bag", true);
-  RandomConditionalProbabilities flavor_params(RandomBoolean("Flavor", true),
-      bag);
-  params_table.insert(
-      make_pair("Flavor", &bn.add_conditional_dirichlet(flavor_params, alpha)));
-  RandomConditionalProbabilities wrapper_params(RandomBoolean("Wrapper", true),
-      bag);
-  params_table.insert(
-      make_pair("Wrapper",
-          &bn.add_conditional_dirichlet(wrapper_params, alpha)));
-  RandomConditionalProbabilities hole_params(RandomBoolean("Hole", true), bag);
-  params_table.insert(
-      make_pair("Hole", &bn.add_conditional_dirichlet(hole_params, alpha)));
-  ConstantNode<DirichletProcessParameters>& bag_params_node =
-      bn.add_dirichlet_process_parameters(bag.name(), 2.0,
-          boost::adaptors::values(params_table));
+public:
 
-  // Read the data from the file
-  CsvMapReader reader(options_map["data-file"].as<string>());
-  CsvMapReader::Records full_data = reader.read_rows();
-
-  // Fill the data in the net
-  size_t line = 0;
-  CsvMapReader::Records::iterator r = full_data.begin();
-  for (; r != full_data.end() && line != lines_of_evidence; ++r, ++line)
+  InfiniteBagFixture()
+      : alpha(5.0)
   {
-    RandomBoolean bag_evidence("Bag", r->operator[]("Bag"));
-    DirichletProcessNode& bag_evidence_node = bn.add_dirichlet_process(
-        bag_params_node);
+    BOOST_REQUIRE_MESSAGE(options_map.count("data-file") == 1,
+        "Please provide test data with the option --data-file.");
+    data_file_ = options_map["data-file"].as<string>();
 
-    for (CsvMapReader::NamedAttributes::iterator a = r->begin(); a != r->end();
-        ++a)
+    correct_flavor_distribution_ = flavor_counts();
+    for (auto entry = correct_flavor_distribution_.begin();
+        entry != correct_flavor_distribution_.end(); ++entry)
     {
-      if (a->first != "Bag")
-      {
-        RandomBoolean var(a->first, a->second);
-        ConditionalCategoricalNode& node = bn.add_conditional_categorical(var,
-          { &bag_evidence_node }, *params_table[a->first]);
-        node.is_evidence(true);
-      }
+      entry->second += alpha;
     }
+    correct_flavor_distribution_.normalize();
   }
 
-  return bn;
-}
+  /**
+   * Provides the number of lines in the file @c data_file_ for the
+   * following conditions:
+   * - C(F=0 | H=1, W=1) (grep -e ",false,true,true" latent-bag.csv | wc) and
+   * - C(F=1 | H=1, W=1) (grep -e ",true,true,true" latent-bag.csv | wc).
+   * The counts were determined with the command in parenthesis (an example
+   * for the file latent-bag.csv).
+   *
+   * The counts (e.g., 52/20) together with the prior parameters (e.g., 5/5)
+   * lead to the total count (57/25). Normalizing the total count leads to
+   * the conditional probabilities of the given file.
+   */
+  initializer_list<CategoricalDistribution::value_type>
+  flavor_counts()
+  {
+    const RandomBoolean flavor_false("Flavor", false);
+    const RandomBoolean flavor_true("Flavor", true);
 
-template<class K, class V>
-ostream&
-put_out_map(ostream& os, const cont::map<K, V>& m)
+    if (data_file_ == "latent-bag-short.csv")
+      return
+      {
+        { flavor_false, 6.0},
+        { flavor_true, 2.0} //
+      };
+
+    else if (data_file_ == "latent-bag.csv")
+      /*
+       * The counts in latent-bag.csv are
+       *   C(F=0 | H=1, W=1): 52 (grep -e ",false,true,true" latent-bag.csv | wc)
+       *   C(F=1 | H=1, W=1): 20 (grep -e ",true,true,true" latent-bag.csv | wc)
+       */
+      return
+      {
+        { flavor_false, 52.0},
+        { flavor_true, 20.0} //
+      };
+
+    else if (data_file_ == "latent-bag-long.csv")
+      return
+      {
+        { flavor_false, 2534.0},
+        { flavor_true, 1280.0} //
+      };
+
+    else
+      return
+      {};
+  }
+
+  BayesianNetwork
+  gen_infinite_bag_net(
+      size_t lines_of_evidence = std::numeric_limits<std::size_t>::max())
+  {
+    const DiscreteRandomReferences no_condition;
+    BayesianNetwork bn;
+
+    // Set up the parameter vertices
+    map<string, ConditionalDirichletNode*> params_table;
+    RandomBoolean bag("Bag", true);
+    RandomConditionalProbabilities flavor_params(RandomBoolean("Flavor", true),
+        bag);
+    params_table.insert(
+        make_pair("Flavor",
+            &bn.add_conditional_dirichlet(flavor_params, alpha)));
+    RandomConditionalProbabilities wrapper_params(
+        RandomBoolean("Wrapper", true), bag);
+    params_table.insert(
+        make_pair("Wrapper",
+            &bn.add_conditional_dirichlet(wrapper_params, alpha)));
+    RandomConditionalProbabilities hole_params(RandomBoolean("Hole", true),
+        bag);
+    params_table.insert(
+        make_pair("Hole", &bn.add_conditional_dirichlet(hole_params, alpha)));
+    ConstantNode<DirichletProcessParameters>& bag_params_node =
+        bn.add_dirichlet_process_parameters(bag.name(), 2.0,
+            boost::adaptors::values(params_table));
+
+    // Read the data from the file
+    CsvMapReader reader(data_file_);
+    CsvMapReader::Records full_data = reader.read_rows();
+
+    // Fill the data in the net
+    size_t line = 0;
+    CsvMapReader::Records::iterator r = full_data.begin();
+    for (; r != full_data.end() && line != lines_of_evidence; ++r, ++line)
+    {
+      RandomBoolean bag_evidence("Bag", r->operator[]("Bag"));
+      DirichletProcessNode& bag_evidence_node = bn.add_dirichlet_process(
+          bag_params_node);
+
+      for (CsvMapReader::NamedAttributes::iterator a = r->begin();
+          a != r->end(); ++a)
+      {
+        if (a->first != "Bag")
+        {
+          RandomBoolean var(a->first, a->second);
+          ConditionalCategoricalNode& node = bn.add_conditional_categorical(var,
+            { &bag_evidence_node }, *params_table[a->first]);
+          node.is_evidence(true);
+        }
+      }
+    }
+
+    return bn;
+  }
+
+  template<class K, class V>
+    ostream&
+    put_out_map(ostream& os, const cont::map<K, V>& m)
+    {
+      for (auto it = m.begin(); it != m.end(); ++it)
+        os << it->first << ", " << it->second << "\n";
+      return os;
+    }
+
+protected:
+
+  const float alpha;
+  CategoricalDistribution correct_flavor_distribution_;
+  string data_file_;
+
+};
+
+BOOST_FIXTURE_TEST_CASE(InfiniteBagTest, InfiniteBagFixture)
 {
-  for (auto it = m.begin(); it != m.end(); ++it)
-    os << it->first << ", " << it->second << "\n";
-  return os;
-}
-
-BOOST_AUTO_TEST_CASE(InfiniteBagTest)
-{
-  BOOST_REQUIRE_MESSAGE(options_map.count("data-file") == 1,
-      "Please provide test data with the option --data-file.");
-
+  /*
+   * Generate the network from the data file.
+   */
   cout << "Generate the bag network with infinite mixtures\n";
   double duration;
   boost::timer t;
-  BayesianNetwork bn = gen_infinite_bag_net(5.0);
+  BayesianNetwork bn = gen_infinite_bag_net();
   duration = t.elapsed();
   if (!options_map["test-mode"].as<bool>())
     cout << "Duration: " << duration << "\n";
@@ -99,6 +180,12 @@ BOOST_AUTO_TEST_CASE(InfiniteBagTest)
   if (options_map["with-debug-output"].as<bool>())
     cout << bn << endl;
 
+  /*
+   * Extend the network for the reasoning task.
+   *
+   * An additional hole and wrapper node serves as evidence for an unknown
+   * flavor node.
+   */
   ConstantNode<DirichletProcessParameters>& bag_params = bn.find<
       ConstantNode<DirichletProcessParameters> >("Bag");
   DirichletProcessNode& bag_v = bn.add_dirichlet_process(bag_params);
@@ -124,6 +211,9 @@ BOOST_AUTO_TEST_CASE(InfiniteBagTest)
   ConditionalCategoricalNode& flavor_v = bn.add_conditional_categorical(flavor,
     { &bag_v }, flavor_params_v);
 
+  /*
+   * Predict the unknown flavor node by sampling.
+   */
   unsigned int burn_in_iterations = options_map["burn-in-iterations"].as<
       unsigned int>();
   unsigned int collect_iterations = options_map["collect-iterations"].as<
@@ -139,16 +229,16 @@ BOOST_AUTO_TEST_CASE(InfiniteBagTest)
     cout << "Duration: " << duration << "\n";
   cout << "Predictive distribution with sampling:\n";
   cout << prediction << endl;
-  /*
-   * The counts in latent-bag.csv are
-   *   C(F=0 | H=1, W=1): 52 (grep -e "false,true,true" bag.csv | wc)
-   *   C(F=1 | H=1, W=1): 20 (grep -e "true,true,true" bag.csv | wc)
-   *
-   * With prior parameters (5, 5) the counts are 57/25. This results in
-   * the probabilities 0.6951/0.3049.
-   */
-  cout << "Correct values (latent-bag.csv):\n";
-  cout << "      (Flavor:0,0.6951)  (Flavor:1,0.3049)\n" << endl;
-  put_out_map(cout, bag_params.value().component_counters());
+
+  if (correct_flavor_distribution_.size() != 0)
+  {
+    cout << "Correct values (" << data_file_ << "):\n";
+    cout << correct_flavor_distribution_ << endl;
+    BOOST_CHECK_CLOSE(prediction.begin()->second,
+        correct_flavor_distribution_.begin()->second, 5);
+  }
+
+  if (options_map["with-debug-output"].as<bool>())
+    put_out_map(cout, bag_params.value().component_counters());
 }
 
