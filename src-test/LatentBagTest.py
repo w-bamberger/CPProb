46589992cef4a9b3f78993d4539ed090ded9dbef
w-bamberger <w.bamberger@arcor.de>
2011-10-20 09:41:47 +0200

    LatentBagTest.py: Improved the test to have the same results as the C++ test.
    * Changed the ordering of the network construction to match that
      of the C++ test. This way, both tests show exactly the same result.
    * Add progress reports and timers.

--- src-test/LatentBagTest.py
+++ src-test/LatentBagTest.py
@@ -3,10 +3,15 @@
 #  or copy at http://www.boost.org/LICENSE_1_0.txt)
 #  Hello World Example from the tutorial
 
-from proby import *
 import csv
+from proby import *
+import sys
+import time
 
 alpha = 5.0
+
+print "Build the network"
+start = time.clock()
 
 bn = BayesianNetwork()
 params_table = dict()
@@ -17,12 +22,12 @@ bag_params_node = bn.add_dirichlet(bag_params, alpha)
 flavor = RandomBoolean("Flavor", True)
 flavor_params = RandomConditionalProbabilities(flavor, bag)
 params_table["Flavor"] = bn.add_conditional_dirichlet(flavor_params, alpha)
-hole = RandomBoolean("Hole", True)
-hole_params = RandomConditionalProbabilities(hole, bag)
-params_table["Hole"] = bn.add_conditional_dirichlet(hole_params, alpha)
 wrapper = RandomBoolean("Wrapper", True)
 wrapper_params = RandomConditionalProbabilities(wrapper, bag)
 params_table["Wrapper"] = bn.add_conditional_dirichlet(wrapper_params, alpha)
+hole = RandomBoolean("Hole", True)
+hole_params = RandomConditionalProbabilities(hole, bag)
+params_table["Hole"] = bn.add_conditional_dirichlet(hole_params, alpha)
 
 csv_reader = csv.DictReader(open("latent-bag.csv"))
 
@@ -35,15 +40,23 @@ for line in csv_reader:
             node = bn.add_conditional_categorical(var, [bag_node], params_table[k])
             node.is_evidence = True
 
-print "Extend the network for prediction"
+print "Duration:", time.clock() - start
 
+print "Extend the network for prediction"
 new_bag_node = bn.add_categorical(bag, bag_params_node)
-new_flavor_node = bn.add_conditional_categorical(flavor, [new_bag_node], params_table["Flavor"])
 new_hole_node = bn.add_conditional_categorical(hole, [new_bag_node], params_table["Hole"])
 new_hole_node.is_evidence = True
 new_wrapper_node = bn.add_conditional_categorical(wrapper, [new_bag_node], params_table["Wrapper"])
 new_wrapper_node.is_evidence = True
+new_flavor_node = bn.add_conditional_categorical(flavor, [new_bag_node], params_table["Flavor"])
 
-print str(bn)
-
-print str(bn.sample(new_flavor_node, 5, 500))
+burn_in_iterations = 5
+collect_iterations = 500
+print "Sample with", burn_in_iterations, "burn-in iterations and", collect_iterations, "collect iterations."
+start = time.clock()
+prediction = bn.sample(new_flavor_node, 5, 500)
+duration = time.clock() - start
+print "Prediction:",
+# prediction already ends with a line break
+sys.stdout.write(str(prediction))   
+print "Duration:", duration
