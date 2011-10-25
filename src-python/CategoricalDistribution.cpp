e6f8b028ebb95463ba214bd57e0bf235adcf84a6
w-bamberger <w.bamberger@arcor.de>
2011-10-25 15:08:49 +0200

    Python CategoricalDistribution: Improved dict interface and statistical functions.
    * Implement parts of the Python dict interface with the new template MapHelper.
    * Export the statistical functions mean, variance, standard_deviation.

--- src-python/CategoricalDistribution.cpp
+++ src-python/CategoricalDistribution.cpp
@@ -1,4 +1,8 @@
+#include "MapHelper.hpp"
 #include <boost/python/class.hpp>
+#include <boost/python/copy_non_const_reference.hpp>
+#include <boost/python/with_custodian_and_ward.hpp>
+#include <boost/python/def.hpp>
 #include <boost/python/operators.hpp>
 #include <cpprob/CategoricalDistribution.hpp>
 
@@ -13,8 +17,34 @@ namespace cpprob
     void
     export_categorical_distribution()
     {
-      class_ < CategoricalDistribution > ("CategoricalDistribution") //
-      .def(self_ns::str(self_ns::self));
+      class_ < CategoricalDistribution
+          > ("CategoricalDistribution") //
+          .def(self_ns::str(self_ns::self)) //
+          .def("__delitem__", &MapHelper < CategoricalDistribution > ::del) //
+          .def("__getitem__", &MapHelper < CategoricalDistribution > ::get,
+              return_value_policy<copy_non_const_reference>()) //
+          .def("__len__", &CategoricalDistribution::size) //
+          .def("__setitem__", &MapHelper < CategoricalDistribution > ::set,
+              with_custodian_and_ward<1, 2>()) // to let container keep value
+          .def("__contains__",
+              &MapHelper < CategoricalDistribution > ::contains) //
+          .def("clear", &CategoricalDistribution::clear) //
+          .def("items", &MapHelper < CategoricalDistribution > ::items) //
+          .def("keys", &MapHelper < CategoricalDistribution > ::keys) //
+          .def("values", &MapHelper < CategoricalDistribution > ::values);
+
+      float
+      (*mean_ptr)(const CategoricalDistribution&) = &mean;
+      def("mean", mean_ptr);
+
+	  float
+	  (*standard_deviation_ptr)(const CategoricalDistribution&) = &standard_deviation;
+	  def("standard_deviation", standard_deviation_ptr);
+
+      float
+      (*variance_ptr)(const CategoricalDistribution&) = &variance;
+      def("variance", variance_ptr);
+
     }
 
   }
---
+++ src-python/MapHelper.hpp
@@ -0,0 +1,83 @@
+#include <boost/python/list.hpp>
+#include <boost/python/tuple.hpp>
+
+namespace cpprob
+{
+  namespace proby
+  {
+
+    template<class MapType>
+      class MapHelper
+      {
+
+      public:
+
+        typedef typename MapType::key_type key_type;
+        typedef typename MapType::mapped_type mapped_type;
+        typedef typename MapType::value_type value_type;
+        typedef typename MapType::const_iterator const_iterator;
+        typedef typename MapType::iterator iterator;
+
+        static void
+        del(MapType& m, const key_type& k)
+        {
+          auto search_it = m.find(k);
+          if (search_it != m.end())
+            m.erase(search_it);
+          else
+            PyErr_SetNone(PyExc_KeyError);
+        }
+
+        static mapped_type&
+        get(MapType& m, const key_type& k)
+        {
+          auto search_it = m.find(k);
+          if (search_it != m.end())
+            return search_it->second;
+          PyErr_SetNone(PyExc_KeyError);
+          boost::python::throw_error_already_set();
+        }
+
+        static boost::python::list
+        items(const MapType& m)
+        {
+          boost::python::list t;
+          for (auto it = m.begin(); it != m.end(); ++it)
+            t.append(boost::python::make_tuple(it->first, it->second));
+          return t;
+        }
+
+        static boost::python::list
+        keys(const MapType& m)
+        {
+          boost::python::list t;
+          for (auto it = m.begin(); it != m.end(); ++it)
+            t.append(it->first);
+          return t;
+        }
+
+        static bool
+        contains(const MapType& m, const key_type& k)
+        {
+          return m.find(k) != m.end();
+        }
+
+        static void
+        set(MapType& m, const key_type& k, const mapped_type& v)
+        {
+          m[k] = v;
+        }
+
+        static boost::python::list
+        values(const MapType& m)
+        {
+          boost::python::list t;
+          for (auto it = m.begin(); it != m.end(); ++it)
+            t.append(it->second);
+          return t;
+        }
+
+      };
+
+  }
+}
