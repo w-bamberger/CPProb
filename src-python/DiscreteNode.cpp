390f2ed10dfb854a7ec9f099cd890794ef7ff2e1
w-bamberger <w.bamberger@arcor.de>
2011-11-26 14:49:00 +0100

    Python DiscreteNode: Add property value to access the random variable.

--- src-python/DiscreteNode.cpp
+++ src-python/DiscreteNode.cpp
@@ -10,10 +10,25 @@ namespace cpprob
   namespace proby
   {
 
+	DiscreteRandomVariable
+	discrete_node_get_value(DiscreteNode& node)
+	{
+	  return node.value();
+	}
+
+	void
+	discrete_node_set_value(DiscreteNode& node, 
+							const DiscreteRandomVariable& value)
+	{
+	  node.value() = value;
+	}
+
     void
     export_discrete_node()
     {
-      class_ < DiscreteNode, boost::noncopyable > ("DiscreteNode", no_init);
+      class_ < DiscreteNode, boost::noncopyable > ("DiscreteNode", no_init) //
+		.add_property("value", &discrete_node_get_value, 
+					  &discrete_node_set_value);
 
       ListListConverter<DiscreteNode*>();
       ListRefVectorConverter<DiscreteNode>();
